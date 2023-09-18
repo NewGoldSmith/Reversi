3d09de45539d96b11c3bc80d9ede7f0a4069d8e1
S.Y <70691930+NewGoldSmith@users.noreply.github.com>
2023-09-18 17:14:45 +0900

    Add files via upload

---
+++ ReversiAlphaBeta/mainAB.h
@@ -0,0 +1,13 @@
+/**
+ * @file mainAB.h
+ * @brief ƒƒCƒ“‚ÌéŒ¾
+ * @author Gold Smith
+ * @date 2023
+ *
+ * Released under the MIT license
+ * https: //opensource.org/licenses/mit-license.php
+ */
+#include "GameAB.h"
+#define _CRTDBG_MAP_ALLOC
+
+#pragma once
