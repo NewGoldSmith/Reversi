cbce3ff673acaefefb48b00deea44101d05935e7
S.Y <70691930+NewGoldSmith@users.noreply.github.com>
2024-02-11 01:47:39 +0900

    Delete TestProject directory

--- TestProject/TestProject.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "pch.h"
-
-#pragma comment(lib,  "../BitManip/" _STRINGIZE($CONFIGURATION) "/BitManip-" _STRINGIZE($CONFIGURATION) ".lib")
-#pragma comment(lib,  "../Debug_fnc/" _STRINGIZE($CONFIGURATION) "/Debug_fnc-" _STRINGIZE($CONFIGURATION) ".lib")
-#pragma comment(lib,  "../Engine/" _STRINGIZE($CONFIGURATION) "/Engine-" _STRINGIZE($CONFIGURATION) ".lib")
